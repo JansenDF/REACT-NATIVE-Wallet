b64ff8d928fffb2a97b6c88a638c1a823dca80c6
JansenDF <jansen.lima.df@gmail.com>
2022-08-10 10:33:27 -0300

    Add constant COLORS and FONTS on folder Styles

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import {ThemeProvider} from 'styled-components/native';
+
+import COLORS from '../src/styles/theme';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text style={styled.font}>Wallet</Text>
-    </View>
+    <ThemeProvider theme={COLORS}>
+      <View>
+        <Text style={styled.font}>Wallet</Text>
+      </View>
+    </ThemeProvider>
   );
 };
 
 const styled = StyleSheet.create({
   font: {
     fontFamily: 'DMSans-Bold',
+    color: COLORS.COLORS.TITLE,
   },
 });
 
